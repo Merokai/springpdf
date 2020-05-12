e0de9b7f3729595bdb36c8148029920b28ab4046
Merokai <amonyak@amostack.ovh>
2020-05-12 13:12:35 +0200

    Fixed SalesRepository bean

--- src/main/java/io/rtx/Application.java
+++ src/main/java/io/rtx/Application.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.XADataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import springfox.documentation.builders.PathSelectors;
@@ -16,6 +18,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, XADataSourceAutoConfiguration.class})
 @EnableSwagger2
 @EnableScheduling
+@EnableJpaRepositories("io.rtx")
 public class Application {
 
     public static void main(String[] args) {
